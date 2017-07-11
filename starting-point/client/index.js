739ef848ca24c9b4d0bd2cc6f761a525e3a22036
Maria Schreiber <meschreiber3@gmail.com>
2017-07-10 21:22:29 -0400

    include provider

--- starting-point/client/index.js
+++ starting-point/client/index.js
@@ -8,10 +8,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Main } from './components';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <Router>
-    <Main />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Main />
+    </Router>
+    </Provider>,
   document.getElementById('app')
 );
